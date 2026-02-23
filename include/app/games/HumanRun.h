7d3ae48155cd6863516ae51770ec1ca40eb3affd
Miki <mtomaszewski1711@gmail.com>
2026-02-23 18:24:33 +0100

    humanRun v2

--- include/app/games/HumanRun.h
+++ include/app/games/HumanRun.h
@@ -2,7 +2,56 @@
 #define HUMAN_RUN_H
 
 #include "app/runtimeApplication.hpp"
+enum CustomCharacter {
+  STANDING_HUMAN,
+  RUNNING_HUMAN,
+  JUMPING_HUMAN,
+  TOP_PART,
+  BOTTOM_PART,
+  OBSTACLE1,
+  OBSTACLE2
+};
 
+enum PlayerState {
+  RUNNING1,
+  RUNNING2,
+  JUMP_START,
+  JUMP,
+  JUMP_DOWN
+};
+enum ProgramState {
+  SELECTION,
+  GAME
+};
+struct RunObstacle {
+  bool isActive = false;
+  CustomCharacter type = OBSTACLE1;
+  int x = 15;
+};
+
+struct Player {
+  PlayerState state = RUNNING1;
+  int posX = 2;
+  int score = 0;
+};
+
+struct Game {
+  unsigned long mainTaskTimer = 0;
+  int mainTaskDelay = 700;
+  bool isGameOver = false;
+  int ticksToGenerateNewObst = 4;
+  RunObstacle obstacles[5];
+  bool stateCompleted = false;
+  int ticksToSpeedUp = 5;
+  Player player;
+  int boardBeginX = 0;
+  int boardEndX = 15;
+  int buttonPin = 10;
+};
+enum Mode {
+  SINGLE,
+  DUEL
+};
 class HumanRun : public RuntimeApplication
 {
 public:
@@ -21,6 +70,22 @@ public:
     void udpDataReceived(int messageID, std::vector<uint8_t> data);
     String getAppNameString();
     uint16_t getBackgroundColor();
+private:
+    Game gameArray[2];
+    int gamesOverCount = 0;
+    ProgramState programState = SELECTION;
+    Mode gameMode = SINGLE;
+
+    bool isObstOnPlayerX(Game& game) ;
+    bool isCollision(Game& game);
+    void resetGameOver(Game& game);
+    void processObstacleMove(Game& game);
+    void processHumanMove(Game& game);
+    void initializeGame(Game& game, int begin, int end, int btnPin);
+    void processButton(Game& game);
+    void processScore(Game& game);
+    void generateObstacle(Game& game);
+    void processGame(Game& game);
 };
 
 #endif // PONG_H
--- src/app/games/HumanRun.cpp
+++ src/app/games/HumanRun.cpp
@@ -2,36 +2,35 @@
 
 HumanRun::HumanRun()
 {
-
 }
 
 void HumanRun::start(int w, int h)
 {
-
+    initializeGame(gameArray[0], 0, 15, 10);
 }
 
 void HumanRun::input(InputType input)
 {
-
+    if (input == InputType::BUTTON_C)
+    {
+        processButton(gameArray[0]);
+    }
 }
 
 void HumanRun::longPressInput(InputType input)
 {
-
 }
 
 void HumanRun::longPressRelease(InputType input)
 {
-
 }
 
 void HumanRun::analogInput(int x, int y)
 {
-
 }
 
-void HumanRun::udpDataReceived(int messageID, std::vector<uint8_t> data) {
-
+void HumanRun::udpDataReceived(int messageID, std::vector<uint8_t> data)
+{
 }
 
 void HumanRun::touchInput(int x, int y)
@@ -40,24 +39,35 @@ void HumanRun::touchInput(int x, int y)
 
 void HumanRun::update()
 {
-
-
+    static long timer = 0;
+    if (millis() - timer > 500)
+    {
+        int gamesCount = (gameMode == DUEL) ? 2 : 1;
+        for (int i = 0; i < gamesCount; i++)
+        {
+            processGame(gameArray[i]);
+        }
+        timer = millis();
+    }
 }
 
 void HumanRun::render(DisplayProvider &display)
 {
-
+    for (int i = 0; i < 5; i++)
+    {
+        if (gameArray[0].obstacles[i].isActive)
+        {
+            display.fillCircle(gameArray[0].obstacles[i].x, 10, 5, TFT_BLUE);
+        }
+    }
 }
 
 void HumanRun::forceRender(DisplayProvider &display)
 {
-
 }
-
 
 void HumanRun::end()
 {
-
 }
 
 String HumanRun::getAppNameString()
@@ -70,3 +80,189 @@ uint16_t HumanRun::getBackgroundColor()
     return TFT_BLACK;
 }
 
+bool HumanRun::isObstOnPlayerX(Game &game)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (game.obstacles[i].x == game.player.posX)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool HumanRun::isCollision(Game &game)
+{
+    if (isObstOnPlayerX(game) && (game.player.state == RUNNING1 || game.player.state == RUNNING2))
+    {
+        return true;
+    }
+    return false;
+}
+
+void HumanRun::resetGameOver(Game &game)
+{
+    game.player.score = 0;
+    game.player.state = RUNNING1;
+    for (int i = 0; i < 5; i++)
+    {
+        game.obstacles[i].x = game.boardEndX;
+        game.obstacles[i].isActive = false;
+    }
+    gamesOverCount--;
+    game.isGameOver = false;
+}
+
+void HumanRun::processObstacleMove(Game &game)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (game.obstacles[i].isActive == true)
+        {
+            if (game.obstacles[i].x > 0)
+            {
+                game.obstacles[i].x--;
+            }
+            else
+            {
+                game.obstacles[i].x = game.boardEndX;
+                game.obstacles[i].isActive = false;
+            }
+        }
+    }
+}
+
+void HumanRun::processHumanMove(Game &game)
+{
+    switch (game.player.state)
+    {
+    case RUNNING1:
+        game.player.state = RUNNING2;
+        break;
+
+    case RUNNING2:
+        game.player.state = RUNNING1;
+        break;
+
+    case JUMP_START:
+        if (game.stateCompleted == true)
+        {
+            game.player.state = JUMP;
+            game.stateCompleted = false;
+        }
+        else
+        {
+            game.stateCompleted = true;
+        }
+        break;
+
+    case JUMP:
+        if (game.stateCompleted == true)
+        {
+            game.player.state = JUMP_DOWN;
+            game.stateCompleted = false;
+        }
+        else
+        {
+            game.stateCompleted = true;
+        }
+        break;
+
+    case JUMP_DOWN:
+        if (game.stateCompleted == true)
+        {
+            game.player.state = RUNNING1;
+            game.stateCompleted = false;
+        }
+        else
+        {
+            game.stateCompleted = true;
+        }
+        break;
+    }
+}
+
+void HumanRun::initializeGame(Game &game, int begin, int end, int btnPin)
+{
+    game.boardBeginX = begin;
+    game.boardEndX = end;
+    game.player.posX = begin + 2;
+    for (int i = 0; i < 5; i++)
+    {
+        game.obstacles[i].x = game.boardEndX;
+    }
+    game.buttonPin = btnPin;
+}
+
+void HumanRun::processButton(Game &game)
+{
+    if (game.player.state == RUNNING1 || game.player.state == RUNNING2)
+    {
+        game.player.state = JUMP_START;
+    }
+    if (game.isGameOver == true)
+    {
+        resetGameOver(game);
+    }
+}
+
+void HumanRun::processScore(Game &game)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (game.obstacles[i].isActive && game.obstacles[i].x == (game.player.posX - 1))
+        {
+            game.player.score++;
+        }
+    }
+}
+
+void HumanRun::generateObstacle(Game &game)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (game.obstacles[i].isActive == false)
+        {
+            game.obstacles[i].isActive = true;
+            int randValue = random(10);
+            while (randValue < 2 || randValue == 4)
+            {
+                randValue = random(10);
+            }
+            game.ticksToGenerateNewObst = randValue;
+            return;
+        }
+    }
+}
+
+void HumanRun::processGame(Game &game)
+{
+    if ((millis() - game.mainTaskTimer > game.mainTaskDelay) && game.isGameOver == false)
+    {
+        processObstacleMove(game);
+        if (game.ticksToGenerateNewObst == 0)
+        {
+            generateObstacle(game);
+        }
+        game.ticksToGenerateNewObst--;
+        processHumanMove(game);
+        processScore(game);
+        game.isGameOver = isCollision(game);
+        if (game.isGameOver == true)
+        {
+            gamesOverCount++;
+            return;
+        }
+        game.mainTaskTimer = millis();
+        if (game.mainTaskDelay > 300)
+        {
+            game.ticksToSpeedUp--;
+            if (game.ticksToSpeedUp == 0)
+            {
+                game.mainTaskDelay -= 20;
+                game.ticksToSpeedUp = 5;
+            }
+        }
+    }
+}
